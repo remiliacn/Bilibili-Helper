1b37fded4d3461fc2ac401e8efa687201ec1e67d
remiliacn <bilibililvnima@gmail.com>
2019-08-28 13:54:25 -0700

    优化动态爬虫

--- bilibiliService/bilibiliDynamic.py
+++ bilibiliService/bilibiliDynamic.py
@@ -6,6 +6,7 @@ headers = {
 
 class BilibiliDynamic:
     def __init__(self, uuid):
+        self.uid = uuid
         self.baseUrl = 'https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?visitor_uid=0&host_uid=%s&offset_dynamic_id=0' % uuid
         self.contentDict = self._getDict()
         self.lastContent = self._getLastContent()
@@ -18,30 +19,46 @@ class BilibiliDynamic:
             return {'-1' : ''}
 
         json_data = json.loads(page)
-        if json_data['data']['has_more'] == 0:
+        if len(json_data['data']) == 0 or json_data['data']['has_more'] == 0:
             return {'-1' : ''}
 
         return json.loads(json_data['data']['cards'][0]['card'])
 
     def _getLastContent(self):
-        if '-1' in self.contentDict:
-            return ''
         response = ''
-        if 'content' in self.contentDict['item']:
+
+        if '-1' in self.contentDict:
+            response += 'emmmm这个uid为%d的人好像没有发布任何动态呢' % self.uid
+
+        elif 'videos' in self.contentDict and self.contentDict['videos'] >= 1:
+            response += '发布了一个视频，标题为：%s' % self.contentDict['title']
+
+        elif 'intro' in self.contentDict and len(self.contentDict['intro']) > 0:
+            response += '发布了一个音乐作品：标题为:%s' % self.contentDict['title']
+
+        elif 'sketch' in self.contentDict and len(self.contentDict['sketch']) != 0:
+            response += '发布了一个企划，标题为:%s' % self.contentDict['sketch']['title']
+
+        elif 'content' in self.contentDict['item']:
             content = self.contentDict['item']['content']
             response += content
             originContent = self._getOriginDict()
             if originContent != '':
-                if 'description' in originContent['item']:
+                if 'item' in originContent and 'description' in originContent['item']:
                     response += '\n转发的原文：\n' + originContent['item']['description']
+                elif 'title' in originContent:
+                    response += '\n转发的视频标题：\n' + originContent['title']
                 else:
                     response += '\n转发的原文：\n' + originContent['item']['content']
+
         else:
             if 'description' in self.contentDict['item']:
                 response += self.contentDict['item']['description']
             else:
                 response += self.contentDict['item']['content']
 
+        if response == '':
+            response += 'emmmm这个uid为%d的人好像没有发布任何动态呢' % self.uid
         return response
 
     def _getOriginDict(self):
