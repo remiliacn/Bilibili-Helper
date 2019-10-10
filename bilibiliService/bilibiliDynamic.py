71773a38eb87d3f26cb9423f43fc291e82365e24
YuxuanZuo <yuxuan.zuo@outlook.com>
2019-10-10 09:42:17 +0800

    add dynamic pictures download function

--- bilibiliService/bilibiliDynamic.py
+++ bilibiliService/bilibiliDynamic.py
@@ -1,4 +1,4 @@
-<import requests, json, os
+import requests, json, os
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
